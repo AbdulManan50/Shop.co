3359b75047b9eb626f5590a6eaddbac33196d0fd
AbdulManan50 <mananiqbal10@gmail.com>
2024-12-20 22:17:14 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <>
+      <div><h1 className="text-3xl font-Integral-CF" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam modi earum quasi natus expedita sunt blanditiis dolore neque ab vitae maiores error laudantium deleniti provident aut id, odio, amet et.</h1></div>
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "Integral-CF": ["'FONTSPRING DEMO - Integral-CF'", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
